1071b98f11d2ccc3d8d9b7286a7dd5d3ea8554a0
Vlad Stolyarov <vlad.stolyarov@gmail.com>
2016-07-18 15:18:49 +0100

    Logging server capabilities

---
+++ common/sip_common/logging_server.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+"""
+An example of logging server which uses socketserver module, taken from
+https://docs.python.org/3/howto/logging-cookbook.html#configuration-server-example .
+The host name is taken from SIP_HOSTNAME environment variable. 
+The value of logging.handlers.DEFAULT_TCP_LOGGING_PORT is 9020 .
+
+The server listens to the port logging.handlers.DEFAULT_TCP_LOGGING_PORT
+where different modules snd their logs, and redirect them to stdout .
+
+"""
+
+import pickle
+import logging
+import logging.handlers
+import socketserver
+import struct
+import os
+
+
+class LogRecordStreamHandler(socketserver.StreamRequestHandler):
+    """Handler for a streaming logging request.
+
+    This basically logs the record using whatever logging policy is
+    configured locally.
+    """
+
+    def handle(self):
+        """
+        Handle multiple requests - each expected to be a 4-byte length,
+        followed by the LogRecord in pickle format. Logs the record
+        according to whatever policy is configured locally.
+        """
+        while True:
+            chunk = self.connection.recv(4)
+            if len(chunk) < 4:
+                break
+            slen = struct.unpack('>L', chunk)[0]
+            chunk = self.connection.recv(slen)
+            while len(chunk) < slen:
+                chunk = chunk + self.connection.recv(slen - len(chunk))
+            obj = self.unPickle(chunk)
+            record = logging.makeLogRecord(obj)
+            self.handleLogRecord(record)
+
+    def unPickle(self, data):
+        return pickle.loads(data)
+
+    def handleLogRecord(self, record):
+        # if a name is specified, we use the named logger rather than the one
+        # implied by the record.
+        if self.server.logname is not None:
+            name = self.server.logname
+        else:
+            name = record.name
+        logger = logging.getLogger(name)
+        # N.B. EVERY record gets logged. This is because Logger.handle
+        # is normally called AFTER logger-level filtering. If you want
+        # to do filtering, do it at the client end to save wasting
+        # cycles and network bandwidth!
+        logger.handle(record)
+
+class LogRecordSocketReceiver(socketserver.ThreadingTCPServer):
+    """
+    Simple TCP socket-based logging receiver suitable for testing.
+    """
+
+    allow_reuse_address = True
+
+    def __init__(self, host=os.environ['SIP_HOSTNAME'],
+                 port=logging.handlers.DEFAULT_TCP_LOGGING_PORT,
+                 handler=LogRecordStreamHandler):
+        socketserver.ThreadingTCPServer.__init__(self, (host, port), handler)
+        self.abort = 0
+        self.timeout = 1
+        self.logname = None
+
+    def serve_until_stopped(self):
+        import select
+        abort = 0
+        while not abort:
+            rd, wr, ex = select.select([self.socket.fileno()],
+                                       [], [],
+                                       self.timeout)
+            if rd:
+                self.handle_request()
+            abort = self.abort
+
+def main():
+    logging.basicConfig(
+        format='%(relativeCreated)5d %(name)-15s %(levelname)-8s %(message)s')
+    tcpserver = LogRecordSocketReceiver()
+    print('About to start TCP server on ' + os.environ['SIP_HOSTNAME'] + '...')
+    try:
+        tcpserver.serve_until_stopped()
+    except KeyboardInterrupt:
+        tcpserver.shutdown()
+
+if __name__ == '__main__':
+    main()
