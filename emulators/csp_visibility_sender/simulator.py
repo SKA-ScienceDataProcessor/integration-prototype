d23b8607fedbb9ee51bec9fed143c4a09fe77566
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2016-11-04 17:33:02 +0000

    Modified sender to work with test receiver.

--- emulators/csp_visibility_sender/simulator.py
+++ emulators/csp_visibility_sender/simulator.py
@@ -11,12 +11,12 @@ class Simulator(object):
 
     def __init__(self, config, log):
         _obs = config['observation']
-        _tel = _obs['telescope']
         self.log = log
         self.config = config
-        self.num_baselines = _tel['num_baselines']
-        self.num_times = _obs['time']['num_times']
-        self.total_channels = _obs['frequency']['num_channels']
+        self.num_stations = _obs['num_stations']
+        self.num_baselines = self.num_stations * (self.num_stations - 1) // 2
+        self.num_times = _obs['num_times']
+        self.total_channels = _obs['num_channels']
         self.sender_start_channel = config['sender_node']['start_channel']
         self.num_streams = len(config['sender_node']['streams'])
         self.stream_num_channels = self.total_channels // self.num_streams
@@ -51,7 +51,7 @@ class Simulator(object):
                 streamer.payload['channel_baseline_count'] = \
                     [(self.stream_num_channels, 0)]
                 streamer.payload['channel_baseline_id'] = [(c0, 0)]
-                vis_data = np.ones(self.frame_shape, dtype=np.complex64)
+                vis_data = np.ones(self.frame_shape, dtype='c8')
                 self.log.debug('>> Channels = {} <<'.format(range(c0, c1)))
                 for c in range(c0, c1):
                     vis_data[:, :, c, :, :].real = t
