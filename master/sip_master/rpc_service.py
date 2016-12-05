1bbab5ea7d70786beba97d0c3f856c86ae03d5ce
Benjamin Mort <benjamin.mort@oerc.ox.ac.uk>
2016-12-05 12:26:22 +0000

    Updated docstrings.

--- master/sip_master/rpc_service.py
+++ master/sip_master/rpc_service.py
@@ -1,40 +1,67 @@
-""" Master controller rpyc server
-"""
+# -*- coding: utf-8 -*-
+"""Master controller rpyc server"""
 __author__ = 'Brian McIlwrath'
 
 import rpyc
 from sip_common import logger
 from sip_master import config
 
-""" This is a rpyc service where the commands starting with 'exposed_'
-    are available to the client - less the 'exposed_' text
-    Example client code:
-       conn=rpyc.connect('localhost',port=12345)
-       result = conn.root.offline()
-
-A (tpd) command returning a value with client arguments
-       retval=conn.root.tpd_command(arg1,arg2,arg3)
-"""
 
 class RpcService(rpyc.Service):
-   def on_connect(self):
-      logger.info(" master controller client controller connected")
+    """Master Controller RPC control interface.
 
-   def on_disconnect(self):
-      logger.info("master controller client controller disconnected")
+    This is an rpyc service where the commands starting with 'exposed_'
+    are available to the client - less the 'exposed_' text
 
-   def exposed_online(self,callback=None):
-      return config.state_machine.post_event(['online'])
+    Example client code:
+    ::
+        conn=rpyc.connect('localhost',port=12345)
+        result = conn.root.offline()
 
-   def exposed_capability(self, name, type, callback=None):
-      return config.state_machine.post_event(['cap', name, type])
+    A (tpd) command returning a value with client arguments
+    ::
+        retval=conn.root.tpd_command(arg1,arg2,arg3)
+    """
+    def on_connect(self):
+        """Called when connecting to the RPC service."""
+        logger.info(" master controller client controller connected")
 
-   def exposed_offline(self,callback=None):
-      return config.state_machine.post_event(['offline'])
+    def on_disconnect(self):
+        """Called when disconnecting from the RPC service."""
+        logger.info("master controller client controller disconnected")
 
-   def exposed_shutdown(self,callback=None):
-      return config.state_machine.post_event(['shutdown'])
+    def exposed_online(self, callback=None):
+        """Exposed online method.
+        Sends the :code:`online` command to the Master Controller state machine."""
+        return config.state_machine.post_event(['online'])
 
-   def exposed_get_current_state(self):
-      return config.state_machine.current_state()
+    def exposed_capability(self, name, type, callback=None):
+        """Exposed capability method.
+        Sends the :code:`cap` command to the Master Controller state machine.
+
+        Args:
+            name (str): The name of the capability.
+            type (str): The type of the capability.
+        """
+        return config.state_machine.post_event(['cap', name, type])
+
+    def exposed_offline(self,callback=None):
+        """Exposed capability method.
+        Sends the :code:`cap` command to the Master Controller state machine."""
+        return config.state_machine.post_event(['offline'])
+
+    def exposed_shutdown(self,callback=None):
+        """Exposed shutdown method.
+        Sends the :code:`shutdown` command to the Master Controller state
+        machine."""
+        return config.state_machine.post_event(['shutdown'])
+
+    def exposed_get_current_state(self):
+        """Exposed get_current_state method.
+        Returns the current state of Master Controller state machine.
+
+        Returns:
+            str: Master Controller state
+        """
+        return config.state_machine.current_state()
 
