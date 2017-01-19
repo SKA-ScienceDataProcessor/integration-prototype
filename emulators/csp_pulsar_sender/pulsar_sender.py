c69fec3db08ceada26414d366531aac79ad69df6
njthykkathu <nijin.thykkathu@stfc.ac.uk>
2017-01-19 15:41:41 +0000

    Structured the PrsSender class into a more efficient way

--- emulators/csp_pulsar_sender/pulsar_sender.py
+++ emulators/csp_pulsar_sender/pulsar_sender.py
@@ -1,31 +1,29 @@
+# -*- coding: utf-8 -*-
 import json
 from ftplib import FTP
 
-class prs_sender:
+"""Module to send pulsar data, FTP client."""
+__author__ = 'Nijin Thykkathu'
 
+
+class PrsSender:
     def __init__(self):
+        # Initialise ftp client
         self._host = 'localhost'
         self._port = 7878
         self._ftp = FTP()
         self._ftp.connect(self._host, self._port)
         self._ftp.login(user='user', passwd='12345')
         self._ftp.set_pasv(False)
-        #self._ftp.sendcmd('MODE B')
+        # self._ftp.sendcmd('MODE B')
 
-    def send(self, obs_id, beam_id):
-        meta = {'metadata': { 'name': 'candidate one',
-            'observation_id': 1,
-            'beam_id': 1 },
-            'data': { 'n_channels': 1000,
-            'n_sub_integrations': 1000}}
-
+    def send(self, config, obs_id, beam_id):
+        # Two sets of file are send
         socket = self._ftp.transfercmd('STOR {0}_{1}'.format(obs_id, beam_id))
-        socket.send(json.dumps(meta).encode())
+        socket.send(json.dumps(config).encode())
         socket.send(bytearray(1000 * 1000))
-        meta['metadata']['name'] = 'candidate two'
-        socket.send(json.dumps(meta).encode())
+        # Changing the name of the metadata - For testing
+        config['metadata']['name'] = 'candidate two'
+        socket.send(json.dumps(config).encode())
         socket.send(bytearray(1000 * 1000))
         socket.close()
-
-if __name__ == '__main__':
-    prs_sender().send(1, 1)
