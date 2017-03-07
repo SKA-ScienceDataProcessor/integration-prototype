80d7c6225a41882db5f22c99634d4d3d84b7c549
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2017-03-07 16:30:47 +0000

    Changed all sys.path.append to prepend using sys.path.insert(0, ...)
    Hopefully this will fix problems encountered on some systems.

--- sip/slave/__main__.py
+++ sip/slave/__main__.py
@@ -12,7 +12,7 @@ import threading
 import time
 from rpyc.utils.server import ThreadedServer
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
 from sip.slave import config
 
 
--- sip/tasks/vis_receiver_task.py
+++ sip/tasks/vis_receiver_task.py
@@ -11,7 +11,7 @@ import sys
 import signal
 import simplejson as json
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
 
 from sip.processor_software.vis_receiver import VisReceiver
 from sip.common.logging_api import log
