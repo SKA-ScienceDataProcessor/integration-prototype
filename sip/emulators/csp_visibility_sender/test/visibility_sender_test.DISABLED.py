9e934d36b6bdf2fdddbd1038008d5003b4193b55
Benjamin Mort <ben.mort@gmail.com>
2017-09-20 23:48:48 +0100

    Disabled the visibility emulator test as this depends on the python spead library. This will be moved out of SIP core code.

--- sip/emulators/csp_visibility_sender/test/visibility_sender_test.DISABLED.py
+++ sip/emulators/csp_visibility_sender/test/visibility_sender_test.DISABLED.py
@@ -15,9 +15,10 @@ import sys
 import numpy as np
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', '..',
-    '..'))
+                                '..'))
 
 from sip.emulators.csp_visibility_sender.heap_streamer import HeapStreamer
+
 
 class TestHeapSimulator:
 
