ca1f5e829919b80f4cf947233f22b8196917f2f8
Benjamin Mort <ben.mort@gmail.com>
2017-09-30 02:08:22 +0100

    fixed a number of linter errors

--- sip/tests/test_execution.py
+++ sip/tests/test_execution.py
@@ -5,13 +5,12 @@
 
 Based on sip_run.py
 """
-
 import subprocess
-import rpyc
 import time
 import select
-
 import unittest
+
+import rpyc
 
 DEBUG = False
 
@@ -23,10 +22,11 @@ class TestSipRunner(unittest.TestCase):
     """
 
     def __init__(self):
-        """Sets up a command line parser, initialises logging and runs the
+        """ Initialises the tests.
+
+        Sets up a command line parser, initialises logging and runs the
         specified module.
         """
-
         # Start master controller
         self._init_master()
 
@@ -69,20 +69,20 @@ class TestSipRunner(unittest.TestCase):
 
         _args = []
         steps = [
-                    ('get_current_state', None, []),
-                    ('online', 'Available', []),
-                    ('capability', None, ['receiver', 'vis_receive'],
-                        self.run_vis, ('sip/etc/vis_sender_config.json')),
-                    ('capability', None, ['receiver', 'vis_receive'],
-                        self.vis_timeout),
-                    ('offline', 'Standby', []),
-                    ('shutdown', None, [])
-                ]
+            ('get_current_state', None, []),
+            ('online', 'Available', []),
+            ('capability', None, ['receiver', 'vis_receive'],
+             self.run_vis, ('sip/etc/vis_sender_config.json')),
+            ('capability', None, ['receiver', 'vis_receive'],
+             self.vis_timeout),
+            ('offline', 'Standby', []),
+            ('shutdown', None, [])
+            ]
 
         for step in steps:
             function = getattr(conn.root, step[0])
             ret = function(*step[2])
-            print('>> ', step[0],':', ret)
+            print('>> ', step[0], ':', ret)
 
             if len(step) > 3:
                 if len(step) > 4:
@@ -96,7 +96,7 @@ class TestSipRunner(unittest.TestCase):
                     if count == 10:
                         print('Encountered error in [', step[0], '], terminating')
                         return -1
-                    count += 1;
+                    count += 1
                     function = getattr(conn.root, 'get_current_state')
                     _args = []
                     ret = function(*_args)
@@ -104,35 +104,40 @@ class TestSipRunner(unittest.TestCase):
                     time.sleep(1)
 
     def _init_master(self):
+        """."""
         print('Starting master controller, waiting to initialise...')
         self._master = subprocess.Popen(['/usr/bin/python', '-m', 'sip.master'],
-                stdin=subprocess.PIPE,stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE)
+                                        stdin=subprocess.PIPE,
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE)
 
         self.poller = select.poll()
-        self.poller.register(self._master.stdout,select.POLLIN)
+        self.poller.register(self._master.stdout, select.POLLIN)
 
-        #TODO properly determine startup complete
+        # TODO properly determine startup complete
         self.poll_master_stdout(5, '** Enter command:', '_init_master')
-   
+
     def shutdown_master(self):
+        """."""
         print('Waiting for master to shut down.')
         #self.poll_master_stdout(5, 'Shutdown complete. Goodbye!', 'shutdown_master')
         self.poll_master_stdout(5, 'SIGINT', 'shutdown_master')
 
         self._master.wait()
         final_msgs = self._master.communicate()
-        #TODO print?
+        # TODO print?
         if DEBUG:
             print(final_msgs)
 
     def run_vis(self, config):
+        """."""
         self.poll_master_stdout(5, 'Waiting to receive', 'run_vis initialisation')
 
         proc = subprocess.Popen(['/usr/bin/python', '-m',
-            'sip.emulators.csp_visibility_sender', config],
-                stdin=subprocess.DEVNULL,stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE)
+                                 'sip.emulators.csp_visibility_sender', config],
+                                stdin=subprocess.DEVNULL,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
         proc.wait()
         output = proc.communicate()
         if DEBUG:
@@ -144,11 +149,12 @@ class TestSipRunner(unittest.TestCase):
         self.poll_master_stdout(5, 'Received heap 9', 'run_vis termination')
 
     def vis_timeout(self):
+        """."""
         self.poll_master_stdout(60, 'vis_receiver_task.py timed out',
-        'vis_timeout')
+                                'vis_timeout')
 
     def poll_master_stdout(self, timeout, trigger, errstring):
-
+        """."""
         output = ''
         timer = 0
 
@@ -175,7 +181,6 @@ class TestSipRunner(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    runner = TestSipRunner()
-    runner.test_master_rpc()
-    runner.shutdown_master()
-
+    RUNNER = TestSipRunner()
+    RUNNER.test_master_rpc()
+    RUNNER.shutdown_master()
