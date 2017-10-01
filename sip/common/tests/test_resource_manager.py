969f368cc58d0be28236170c1ca67a1465abc0c8
Benjamin Mort <ben.mort@gmail.com>
2017-10-01 12:59:58 +0100

    - Fixed linter errors in the resource manager and state machine tests.

--- sip/common/tests/test_resource_manager.py
+++ sip/common/tests/test_resource_manager.py
@@ -1,105 +1,131 @@
+# coding: utf-8
+""" Tests of the resource manager.
+"""
 import unittest
 
 from sip.common.resource_manager import ResourceManager
 
 
 class ResourceManagerTest(unittest.TestCase):
+    """ Tests of the ResourceManager
+    """
+
     def setUp(self):
+        """ Set up test fixture.
+
+        Creates a ResourceManager object.
+        """
         self.mgr = ResourceManager({
             'localhost': {
-                 'sip_root': 'root1',
-                 'launch_protocol': ['ssh', 'docker']
+                'sip_root': 'root1',
+                'launch_protocol': ['ssh', 'docker']
             },
             'docker': {
-                 'sip_root': 'root2',
-                 'launch_protocol': ['docker']
+                'sip_root': 'root2',
+                'launch_protocol': ['docker']
             },
             'ssh': {
-                 'sip_root': 'root3',
-                 'launch_protocol': ['ssh']
+                'sip_root': 'root3',
+                'launch_protocol': ['ssh']
             }
         })
 
-    def testExplicitHost(self):
-
-        # Test explicit host
+    def test_explicit_host(self):
+        """ Test explicit host
+        """
         self.assertEqual(
             self.mgr.allocate_host('test1', {'host': 'localhost'}, {}),
-                'localhost')
+            'localhost')
         self.assertEqual(
-            self.mgr.allocate_host('test2', {'host': 'docker'}, {}), 'docker')
+            self.mgr.allocate_host('test2', {'host': 'docker'}, {}),
+            'docker')
         with self.assertRaises(RuntimeError):
             self.mgr.allocate_host('test3', {'host': 'xxxxx'}, {})
 
-    def testExclusive1(self):
-
-        # Test the exclusive flag
+    def test_exclusive1(self):
+        """ Test the exclusive flag
+        """
         self.mgr.allocate_host('test1',
-                {'host': 'localhost', 'exclusive': True}, {})
+                               {'host': 'localhost', 'exclusive': True},
+                               {})
         with self.assertRaises(RuntimeError):
             self.mgr.allocate_host('test2', {'host': 'localhost'}, {})
 
-    def testExclusive2(self):
-
-        # Test the exclusive flag
+    def test_exclusive2(self):
+        """ Test the exclusive flag
+        """
         self.mgr.allocate_host('test1', {'host': 'localhost'}, {})
         with self.assertRaises(RuntimeError):
             self.mgr.allocate_host('test2',
-                {'host': 'localhost', 'exclusive': True}, {})
+                                   {'host': 'localhost', 'exclusive': True},
+                                   {})
 
-    def testSelectUnused(self):
-        t1 = self.mgr.allocate_host('test1', {}, {})
-        t2 = self.mgr.allocate_host('test2', {}, {})
-        t3 = self.mgr.allocate_host('test3', {}, {})
-        self.assertNotEqual(t1, t2)
-        self.assertNotEqual(t1, t3)
-        self.assertNotEqual(t2, t3)
-        t4 = self.mgr.allocate_host('test4', {}, {})
+    def test_select_unused(self):
+        """ FIXME(BM): add description
+        """
+        host1 = self.mgr.allocate_host('test1', {}, {})
+        host2 = self.mgr.allocate_host('test2', {}, {})
+        host3 = self.mgr.allocate_host('test3', {}, {})
+        self.assertNotEqual(host1, host2)
+        self.assertNotEqual(host1, host3)
+        self.assertNotEqual(host2, host3)
+        _ = self.mgr.allocate_host('test4', {}, {})
 
-    def testDuplicate(self):
+    def test_duplicate(self):
+        """ FIXME(BM): add description
+        """
         self.mgr.allocate_host('test1', {'host': 'localhost'}, {})
         with self.assertRaises(RuntimeError):
             self.mgr.allocate_host('test1', {}, {})
 
-    def testRelease(self):
+    def test_release(self):
+        """ FIXME(BM): add description
+        """
         self.mgr.allocate_host('test1', {'host': 'localhost'}, {})
         self.mgr.release_host('test1')
         self.mgr.allocate_host('test1', {'host': 'localhost'}, {})
 
-    def testBadResource(self):
+    def test_bad_resource(self):
+        """ FIXME(BM): add description
+        """
         with self.assertRaises(RuntimeError):
             self.mgr.allocate_resource('test1', 'tcp_port')
         self.mgr.allocate_host('test1', {}, {})
         with self.assertRaises(RuntimeError):
             self.mgr.allocate_resource('test1', 'xxxx')
 
-    def testAllocateTcpPort(self):
+    def test_allocate_tcp_port(self):
+        """ FIXME(BM): add description
+        """
         self.mgr.allocate_host('test1', {}, {})
         self.assertEqual(self.mgr.allocate_resource('test1', 'tcp_port'),
-                6000)
+                         6000)
         self.assertEqual(self.mgr.allocate_resource('test1', 'tcp_port'),
-                6001)
+                         6001)
 
-    def testDeallocateTcpPort(self):
+    def test_deallocate_tcp_port(self):
+        """ FIXME(BM): add description
+        """
         self.mgr.allocate_host('test1', {'host': 'localhost'}, {})
         self.assertEqual(self.mgr.allocate_resource('test1', 'tcp_port'),
-                6000)
+                         6000)
         self.mgr.release_host('test1')
         self.mgr.allocate_host('test2', {'host': 'localhost'}, {})
         self.assertEqual(self.mgr.allocate_resource('test2', 'tcp_port'),
-                6000)
+                         6000)
 
-    def testLaunchProtocol(self):
-        self.mgr.allocate_host('test1',
-                {'host': 'localhost', 'exclusive': True}, {})
-        self.assertEqual(self.mgr.allocate_host('test2',
-                {'launch_protocol': 'ssh'}, {}), 'ssh')
-        self.assertEqual(self.mgr.allocate_host('test3',
-                {'launch_protocol': 'docker'}, {}), 'docker')
+    def test_launch_protocol(self):
+        """ FIXME(BM): add description
+        """
+        self.mgr.allocate_host(
+            'test1', {'host': 'localhost', 'exclusive': True}, {})
+        self.assertEqual(self.mgr.allocate_host(
+            'test2', {'launch_protocol': 'ssh'}, {}), 'ssh')
+        self.assertEqual(self.mgr.allocate_host(
+            'test3', {'launch_protocol': 'docker'}, {}), 'docker')
 
-    def testSipRoot(self):
+    def test_sip_root(self):
+        """ FIXME(BM): add description
+        """
         self.assertEqual(self.mgr.sip_root('localhost'), 'root1')
         self.assertEqual(self.mgr.sip_root('docker'), 'root2')
-
-if __name__ == "__main__":
-    unittest.main()
--- sip/common/tests/test_state_machine.py
+++ sip/common/tests/test_state_machine.py
@@ -1,3 +1,6 @@
+# coding: utf-8
+""" Tests of the state machine
+"""
 import unittest
 import time
 
@@ -5,96 +8,126 @@ from sip.common.state_machine import State
 from sip.common.state_machine import TimedState
 from sip.common.state_machine import StateMachine
 
-trace = []
+TRACE = []  # List of state transitions / actions
 
 
 class Offline(State):
-    def __init__(self, sm):
-        trace.append("entering offline")
+    """ Offline state
+    """
+    # pylint: disable=unused-argument
+    def __init__(self, state_machine):
+        TRACE.append("entering offline")
 
     def exit(self):
-        trace.append("exiting offline")
+        """ Exit action
+        """
+        TRACE.append("exiting offline")
 
 
 class Online(State):
-    def __init__(self, sm):
-        trace.append("entering online")
+    """ Online state
+    """
+    # pylint: disable=unused-argument
+    def __init__(self, state_machine):
+        TRACE.append("entering online")
 
     def exit(self):
-        trace.append("exiting online")
+        """ Exit action
+        """
+        TRACE.append("exiting online")
 
 
 class Wait(TimedState):
-    def __init__(self, sm):
-        TimedState.__init__(self, sm, 5, ["timeout"])
-        trace.append("entering wait")
+    """ Wait state
+    """
+    def __init__(self, state_machine):
+        TimedState.__init__(self, state_machine, 1, ["timeout"])
+        TRACE.append("entering wait")
 
     def exit(self):
-        trace.append("exiting wait")
+        """ Exit action
+        """
+        TRACE.append("exiting wait")
 
 
 class TestSM(StateMachine):
+    """ Mock state machine class used for the test.
+
+    Defines a test state machine.
+    """
+    # pylint: disable=unused-argument, no-self-use
+
     def __init__(self):
         super(TestSM, self).__init__(self.state_table, Offline)
 
     def action_offline(self, event_name):
-        trace.append("going offline")
+        """ Offline action
+        """
+        TRACE.append("going offline")
 
     def action_online(self, event_name):
-        trace.append("going online")
+        """ Online action
+        """
+        TRACE.append("going online")
 
     def action_wait(self, event_name):
-        trace.append("waiting")
+        """ Wait action
+        """
+        TRACE.append("waiting")
 
     state_table = {
         'Offline': {
-            'start': (1, Online,  action_online),
+            'start': (1, Online, action_online),
         },
-        'Online' : {
-            'stop' : (1, Offline, action_offline),
-            'wait' : (1, Wait, action_wait)
+        'Online': {
+            'stop': (1, Offline, action_offline),
+            'wait': (1, Wait, action_wait)
         },
-        'Wait' : {
-            'timeout' : (1, Offline, action_offline),
+        'Wait': {
+            'timeout': (1, Offline, action_offline),
         }
-}
+    }
 
 
 class StateMachineTest(unittest.TestCase):
+    """ Tests of the State Machine.
+    """
+
     def setUp(self):
-        self.sm = TestSM()
+        self.state_machine = TestSM()
 
-    def testSimple(self):
-        self.sm.post_event(['start'])
-        self.assertEqual(self.sm.current_state(), 'Online')
-        self.sm.post_event(['start'])
-        self.assertEqual(self.sm.current_state(), 'Online')
-        self.sm.post_event(['stop'])
-        self.assertEqual(self.sm.current_state(), 'Offline')
+    def test_simple(self):
+        """ Test simple default use of the state machine
+        """
+        self.state_machine.post_event(['start'])
+        self.assertEqual(self.state_machine.current_state(), 'Online')
+        self.state_machine.post_event(['start'])
+        self.assertEqual(self.state_machine.current_state(), 'Online')
+        self.state_machine.post_event(['stop'])
+        self.assertEqual(self.state_machine.current_state(), 'Offline')
 
-        self.assertEqual(trace[0], 'entering offline')
-        self.assertEqual(trace[1], 'exiting offline')
-        self.assertEqual(trace[2], 'going online')
-        self.assertEqual(trace[3], 'entering online')
-        self.assertEqual(trace[4], 'exiting online')
-        self.assertEqual(trace[5], 'going offline')
-        self.assertEqual(trace[6], 'entering offline')
+        self.assertEqual(TRACE[0], 'entering offline')
+        self.assertEqual(TRACE[1], 'exiting offline')
+        self.assertEqual(TRACE[2], 'going online')
+        self.assertEqual(TRACE[3], 'entering online')
+        self.assertEqual(TRACE[4], 'exiting online')
+        self.assertEqual(TRACE[5], 'going offline')
+        self.assertEqual(TRACE[6], 'entering offline')
 
-    def testTimer(self):
-        self.sm.post_event(['start'])
-        self.assertEqual(self.sm.current_state(), 'Online')
-        self.sm.post_event(['wait'])
-        self.assertEqual(self.sm.current_state(), 'Wait')
-        time.sleep(10)
-        self.assertEqual(self.sm.current_state(), 'Offline')
+    def test_timer(self):
+        """ Test of timed states (states with a timeout)
+        """
+        self.state_machine.post_event(['start'])
+        self.assertEqual(self.state_machine.current_state(), 'Online')
+        self.state_machine.post_event(['wait'])
+        self.assertEqual(self.state_machine.current_state(), 'Wait')
+        time.sleep(2)
+        self.assertEqual(self.state_machine.current_state(), 'Offline')
 
-        self.assertEqual(trace[0], 'entering offline')
-        self.assertEqual(trace[1], 'exiting offline')
-        self.assertEqual(trace[2], 'going online')
-        self.assertEqual(trace[3], 'entering online')
-        self.assertEqual(trace[4], 'exiting online')
-        self.assertEqual(trace[5], 'going offline')
-        self.assertEqual(trace[6], 'entering offline')
-
-if __name__ == "__main__":
-    unittest.main()
+        self.assertEqual(TRACE[0], 'entering offline')
+        self.assertEqual(TRACE[1], 'exiting offline')
+        self.assertEqual(TRACE[2], 'going online')
+        self.assertEqual(TRACE[3], 'entering online')
+        self.assertEqual(TRACE[4], 'exiting online')
+        self.assertEqual(TRACE[5], 'going offline')
+        self.assertEqual(TRACE[6], 'entering offline')
