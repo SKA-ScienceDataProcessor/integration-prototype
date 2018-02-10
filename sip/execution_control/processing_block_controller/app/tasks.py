6f65663dd5d99dec958e5dc74b0518ee494bf8f5
Benjamin Mort <ben.mort@gmail.com>
2018-02-10 01:35:17 +0000

    added initial skeleton files for Processing Block Controller as a Celery worker

---
+++ sip/execution_control/processing_block_controller/app/tasks.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Processing Block Controller (Celery worker)"""
+import os
+from celery import Celery
+
+
+APP = Celery('tasks', broker=os.getenv('CELERY_BROKER'))
+
+
+@APP.task
+def execute_processing_block(workflow):
+    """Execute a processing block.
+
+    Args:
+        workflow (dict): Workflow description.
+    """
+    pass
