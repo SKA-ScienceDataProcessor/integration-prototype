51735122a69cdc619f1309a1d142de0d49ec55f0
Benjamin Mort <ben.mort@gmail.com>
2018-04-19 13:05:44 +0100

    updated test device server

--- sip/execution_control/tango_db_alt1/test_device_server/register_device.py
+++ sip/execution_control/tango_db_alt1/test_device_server/register_device.py
@@ -2,12 +2,17 @@
 """Python script to register device"""
 from tango import Database, DbDevInfo
 
+# http://pytango.readthedocs.io/en/stable/database.html
+
+db = Database()
+
 # Define the tango Class served by this DServer
 device_info = DbDevInfo()
-device_info.server = 'Test/TestServer'
+device_info.name = 'sip_SDP/test/1'
 device_info._class = 'Test'
-device_info.name = 'sip_SDP/elt/test'
+device_info.server = 'Test/test'
+print('Adding device: %s' % device_info.name)
 
-print('Creating device: %s' % device_info.name)
-db = Database()
+# NOTE: This also adds the server defined by device_info.server to the db
 db.add_device(device_info)
+
