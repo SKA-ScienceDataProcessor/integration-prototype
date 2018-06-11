5f4a8c331fdb0e7db506367fbb63d31259381769
Benjamin Mort <ben.mort@gmail.com>
2018-06-11 18:19:56 +0100

    minor updates due to experimenting / hacking dask interface code

--- deploy/dask_cluster/example_workflow/dask_interface.py
+++ deploy/dask_cluster/example_workflow/dask_interface.py
@@ -6,9 +6,10 @@ import sys
 from pprint import pprint
 from dask.distributed import Client, progress, Scheduler, Executor, Future
 import distributed.scheduler
+import subprocess
 
 # <http://distributed.readthedocs.io/en/latest/api.html#distributed.client.Client.get>
-
+# https://stackoverflow.com/questions/41111889/how-to-terminate-workers-started-by-dask-multiprocessing-scheduler?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 
 def init_logging():
     """."""
@@ -36,6 +37,23 @@ def start(config):
     """Start the workflow stage"""
     # Probably just have to perform a syscall to run the python script and get a
     # processes handle to kill it?
+    pid = subprocess.Popen(['python3', '-m', 'workflow'])
+    print(pid)
+    # In order for this to be stateless, it will be necessary to hold
+    # the PID in the config database?
+    # Could also have the work stopped by tearing down all of the containers
+    # containing the workflow.
+
+    # Options:
+    # 1. Popen
+    # 2. Have the workflow code not need the client and have the client defined
+    #    only in the wrapper. Workflow code is then run against the client.
+    # 3. Pass the client to the workflow
+    # 4. Start a container which contains the workflow and have this run the
+    #    workflow when started. To stop the workflow kill the workflow container
+
+    # --- Number 4 might be the best option.
+
 
 
 def stop(config):
@@ -48,20 +66,44 @@ def stop(config):
 def status(config):
     """Get the status of the workflow stage"""
     # This can be queried more easily by various client functions.
+    # 'PENDING', 'RUNNING', ' FINISHED', 'ERROR', 'WARNING' ?
+    # This can be defined by the adapter
+
+
 
 
 def main():
     """."""
     # <https://distributed.readthedocs.io/en/latest/scheduling-state.html#distributed.scheduler.Scheduler>
 
+    # client = Client('localhost:8786')
+    # client.run(init_logging)
+    # client.run_on_scheduler(init_logging)
+    # start({})
+
+    # Get the list of running futures
     client = Client('localhost:8786')
-    stack = client.call_stack()
-    for s in stack:
-        print(s)
-        for x in stack[s]:
-            print(x)
-            f = Future(x)
-            print(f.done())
+
+    # print(client.processing())
+    p = client.processing()
+    for tasks in p.values():
+        for task in tasks:
+            f = Future(task)
+            f.cancel()
+            print(task, f.done())
+
+
+
+
+    # stack = client.call_stack()
+    # for s in stack:
+    #     while len(stack[s]) > 0:
+    #         print(len(stack[s]))
+    #         for x in stack[s]:
+    #             print(x)
+    #             f = Future(x)
+    #             f.cancel()
+    #             print(f.done())
 
 
     # pprint(client.call_stack())
