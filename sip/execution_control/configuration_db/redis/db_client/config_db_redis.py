ddefff272e48827f9bafba362ccfd93faba296bc
Benjamin Mort <ben.mort@gmail.com>
2018-06-27 11:08:48 +0100

    added decorator for checking connnections to the db. fixed some pydocstyle warnings

--- sip/execution_control/configuration_db/redis/db_client/config_db_redis.py
+++ sip/execution_control/configuration_db/redis/db_client/config_db_redis.py
@@ -1,8 +1,32 @@
 # -*- coding: utf-8 -*-
 """Low Level Configuration Service Client API."""
 import os
+from functools import wraps
+import logging
 
 import redis
+import redis.exceptions
+
+LOG = logging.getLogger('SIP.EC.CDB')
+REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
+REDIS_PORT = os.getenv('REDIS_PORT', 6379)
+REDIS_DB_ID = os.getenv('REDIS_DB_ID', 0)
+
+
+def check_connection(func):
+    """Check connection exceptions."""
+    @wraps(func)
+    def with_exception_handling(*args, **kwargs):
+        """Wrap function being decorated."""
+        try:
+            return func(*args, **kwargs)
+        except redis.exceptions.ConnectionError:
+            raise ConnectionError("Unable to connect to the Redis "
+                                  "Configuration Database. host = {}, "
+                                  "port = {}, id = {}."
+                                  .format(REDIS_HOST, REDIS_PORT,
+                                          REDIS_DB_ID))
+    return with_exception_handling
 
 
 class ConfigDbRedis:
@@ -22,7 +46,7 @@ class ConfigDbRedis:
         self._db.hmset(name, value)
 
     def set_value(self, key, field, value):
-        """Add the state of the key and field"""
+        """Add the state of the key and field."""
         self._db.hset(key, field, value)
 
     def get_specified_values(self, key, field):
@@ -31,15 +55,15 @@ class ConfigDbRedis:
         pass
 
     def get_value(self, key, field):
-        """Get the value associated with the key and field"""
+        """Get the value associated with the key and field."""
         return self._db.hget(key, field)
 
     def get_all_field_value(self, key):
-        """Get all the fields and values stored at key"""
+        """Get all the fields and values stored at key."""
         return self._db.hgetall(key)
 
     def get_list(self, key):
-        """Get all the value in the list"""
+        """Get all the value in the list."""
         return self._db.lrange(key, 0, -1)
 
     def get_element(self, key, index):
@@ -48,24 +72,25 @@ class ConfigDbRedis:
         return self._db.lindex(key, index)
 
     def get_length(self, key):
-        """Get the length of the list stored at key"""
+        """Get the length of the list stored at key."""
         return self._db.llen(key)
 
     def add_element(self, key, element):
-        """Adds a new element to the end of the list"""
+        """Add new element to the end of the list."""
         self._db.lpush(key, element)
 
     def delete_block(self, key):
-        """Delete key"""
+        """Delete key."""
         self._db.delete(key)
 
     def get_all_blocks(self, block_id):
-        """Search all keys associated with the block id"""
+        """Search all keys associated with the block id."""
         key_search = '*' + block_id + '*'
         return self._db.keys(key_search)
 
+    @check_connection
     def get_block(self, block_id):
-        """Search for keys associated with the block id"""
+        """Search for keys associated with the block id."""
         key_search = '*' + block_id
         return self._db.keys(key_search)
 
@@ -80,10 +105,11 @@ class ConfigDbRedis:
         at destination"""
         return self._db.rpoplpush(block_event, block_history)
 
+    @check_connection
     def get_ids(self, pattern):
-        """Search for the key according to the pattern"""
+        """Search for the key according to the pattern."""
         return self._db.keys(pattern)
 
     def flush_db(self):
-        """Clear the entire database"""
+        """Clear the entire database."""
         self._db.flushdb()
