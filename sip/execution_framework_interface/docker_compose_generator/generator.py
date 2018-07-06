2a34538d399250fd17573bcbbbba84819fd5d887
Benjamin Mort <ben.mort@gmail.com>
2018-07-06 17:20:50 +0100

    started fleshing out compose generator module

--- sip/execution_framework_interface/docker_compose_generator/generator.py
+++ sip/execution_framework_interface/docker_compose_generator/generator.py
@@ -1,6 +1,50 @@
 # -*- coding: utf-8 -*-
 """."""
+import os
+
 import jinja2
+
+
+def load_template(file_path, search_paths=None):
+    """Load a Jinja2 template.
+
+    Args:
+        file_path (str): Path of the template file to be loaded. This path
+                         should be relative to a path specified in the
+                         search_paths list.
+        search_paths (list, optional): List of search paths where templates
+                                       are located.
+
+    Returns:
+        (jinja2.Template) Jinja2 template object.
+
+    """
+    if not search_paths:
+        _cwd = os.path.abspath(os.path.dirname(__file__))
+        _dir = os.path.join(_cwd, 'example_templates')
+        search_paths = _dir
+
+    _loader = jinja2.FileSystemLoader(searchpath=search_paths)
+    _env = jinja2.Environment(loader=_loader)
+    return _env.get_template(file_path)
+
+
+def generate_compose_file(workflow_stage_config):
+    """Dynamically generate Docker compose file.
+
+    This convers workflow stage configuration, stored in the Configuration
+    Database, into a Docker Compose file string that can be used to execture
+    a workflow stage.
+    """
+    if 'docker_compose_template' not in workflow_stage_config:
+        raise RuntimeError('Docker Compose template not found in '
+                           'configuration')
+
+    # _cwd = os.path.join(os.path.dirname(__file__))
+    # _dir = os.path.abspath(_cwd, 'example_templates')
+    # template_loader
+
+    return ''
 
 
 def render_compose_template(template, **kwargs):
--- sip/execution_framework_interface/docker_compose_generator/tests/test_generator.py
+++ sip/execution_framework_interface/docker_compose_generator/tests/test_generator.py
@@ -6,19 +6,19 @@ import json
 import yaml
 import jinja2
 
-# from ..generator import render_compose_template
+import pytest
+
+from ..generator import generate_compose_file, load_template
 
 
 def test_generate_sender_compose_file():
     """Test generating a Docker Compose file for visibility send."""
-    # - Load mock scheduler configuration for the visibility sender.
-    # - Workflow stage configuration would normally be in the Configuraiton db
-    #   and loaded by the Processing Controller (Scheduler) or
-    #   Processing Block Controller.
+    # Load workflow stage configuration (for the visibility sender).
+    # This would normally be in the Configuraiton db and loaded by the
+    # Processing Controller (Scheduler) or Processing Block Controller.
     path = os.path.abspath(os.path.dirname(__file__))
     with open(os.path.join(path, 'scheduler_config.yml'), 'r') as _file:
         config = yaml.load(_file)
-    print(config)
     assert 'scheduler_config' in config
     assert config['scheduler_config'][0]['type'] == 'vis_send'
     vis_send_config = config['scheduler_config'][0]['config']
@@ -26,6 +26,7 @@ def test_generate_sender_compose_file():
     assert 'docker_compose_template' in vis_send_config
     assert 'app_config_template' in vis_send_config
 
+    # Load the compose file template specified by the workflow configuration.
     _dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
                                         'example_templates'))
     template_loader = jinja2.FileSystemLoader(searchpath=_dir,
@@ -34,6 +35,7 @@ def test_generate_sender_compose_file():
     path = vis_send_config['docker_compose_template']
     compose_template = template_env.get_template(path)
 
+    # Load the application template specified by the workflow configuration.
     path = vis_send_config['app_config_template']
     app_config_template = template_env.get_template(path)
 
@@ -64,6 +66,9 @@ def test_generate_sender_compose_file():
 
 def test_generate_receiver_compose_file():
     """Test generating a Docker Compose file for visibility receive."""
+    # Load workflow stage configuration (for the visibility sender).
+    # This would normally be in the Configuraiton db and loaded by the
+    # Processing Controller (Scheduler) or Processing Block Controller.
     path = os.path.abspath(os.path.dirname(__file__))
     with open(os.path.join(path, 'scheduler_config.yml'), 'r') as _file:
         config = yaml.load(_file)
@@ -74,6 +79,7 @@ def test_generate_receiver_compose_file():
     assert 'docker_compose_template' in vis_recv_config
     assert 'app_config' in vis_recv_config
 
+    # Load the compose file template.
     _dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
                                         'example_templates'))
     template_loader = jinja2.FileSystemLoader(searchpath=_dir,
@@ -82,15 +88,18 @@ def test_generate_receiver_compose_file():
     path = vis_recv_config['docker_compose_template']
     compose_template = template_env.get_template(path)
 
+    # Load application configuration.
     path = vis_recv_config['app_config']
     with open(os.path.join(_dir, path)) as _file:
         app_config = json.load(_file)
 
+    # Render the compose file template.
     template_params = dict(json_config=app_config,
                            buffer_path=vis_recv_config['buffer_path'],
                            num_receivers=vis_recv_config['num_receivers'])
     compose_file = compose_template.render(**template_params)
 
+    # Verify the template was rendered correctly.
     compose_dict = yaml.load(compose_file)
     assert compose_dict['version'] == '3.6'
     assert 'services' in compose_dict
@@ -101,3 +110,33 @@ def test_generate_receiver_compose_file():
     # as a string to the Docker Swarm EE interface.
     # with open('test_recv.yml', 'w') as _file:
     #     _file.write(compose_file)
+
+
+def test_generate_compose_file_invalid_config():
+    """."""
+    with pytest.raises(RuntimeError):
+        _ = generate_compose_file(dict())
+
+
+def test_load_template():
+    """."""
+    template = load_template('docker-compose.recv.j2.yml')
+    assert isinstance(template, jinja2.Template)
+    assert os.path.isfile(template.filename)
+    # print(vars(template.module))
+    # print(template.module.version)
+
+    with pytest.raises(jinja2.exceptions.TemplateNotFound):
+        _ = load_template('does_not_exist.j2')
+
+
+def test_generate_sender_compose_file_2():
+    """Test generating a Docker Compose file for visibility send."""
+    path = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(path, 'scheduler_config.yml'), 'r') as _file:
+        config = yaml.load(_file)
+    config = config['scheduler_config'][0]['config']
+
+    # config = {}
+    compose_file = generate_compose_file(config)
+    print(compose_file)
