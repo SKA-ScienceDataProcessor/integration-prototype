3ef739f76894596eacb34bba7f6d663b71210839
Benjamin Mort <ben.mort@gmail.com>
2018-07-13 14:42:43 +0100

    Renamed main scheduler module, updated logging format

--- sip/execution_control/processing_controller_scheduler/celery/app/__main__.py
+++ sip/execution_control/processing_controller_scheduler/celery/app/__main__.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 """Processing Controller Scheduler application main."""
 import logging
-from .scheduler import ProcessingBlockScheduler
+import os
+
+from .scheduler_async import ProcessingBlockScheduler
 
 
 def _init_logger():
     """Initialise the logger."""
     _log = logging.getLogger('sip')
-    _log.setLevel(logging.DEBUG)
     _handler = logging.StreamHandler()
-    _handler.setFormatter(logging.Formatter('%(asctime)s.%(msecs)03d - '
-                                            '%(name)s - '
-                                            '%(levelname).1s - '
-                                            '%(message)s',
-                                            '%Y-%m-%d %H:%M:%S'))
+    fmt = os.getenv('SIP_LOG_FORMAT', '%(asctime)s.%(msecs)03d | '
+                    '%(name)s | %(levelname)-7s | %(message)s')
+    _handler.setFormatter(logging.Formatter(fmt, '%Y-%m-%d %H:%M:%S'))
     _log.addHandler(_handler)
+    _log.setLevel(os.getenv('SIP_PC_LOG_LEVEL', 'DEBUG'))
     return _log
 
 
