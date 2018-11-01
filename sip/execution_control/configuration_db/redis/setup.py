136d7ae3645f25b6e3edfd917d17ec5d78859b2b
Benjamin Mort <ben.mort@gmail.com>
2018-11-01 16:12:57 +0000

    fixed linter errors

--- sip/execution_control/configuration_db/redis/setup.py
+++ sip/execution_control/configuration_db/redis/setup.py
@@ -7,14 +7,14 @@ import os
 def package_files(directory):
     """Get list of data files to add to the package."""
     paths = []
-    for (path, directories, filenames) in os.walk(directory):
+    for (path, _, filenames) in os.walk(directory):
         for filename in filenames:
             paths.append(os.path.join('..', path, filename))
     return paths
 
 
-data_files = package_files('config_db/utils/data')
-schema_files = package_files('config_db/schema')
+DATA_FILES = package_files('config_db/utils/data')
+SCHEMA_FILES = package_files('config_db/schema')
 
 
 setup(name='skasip_config_db',
@@ -23,7 +23,7 @@ setup(name='skasip_config_db',
                   'client library.',
       author='SKA SIP',
       packages=['config_db', 'config_db/utils'],
-      package_data={'': data_files, '': schema_files},
+      package_data={'': DATA_FILES + SCHEMA_FILES},
       include_package_data=True,
       install_requires=['redis', 'jsonschema'],
       zip_safe=False)
