1264f17aefb4d584c9bd9fd78ff2339c5b1d8921
Benjamin Mort <ben.mort@gmail.com>
2018-11-01 15:52:01 +0000

    - Updated setup.py to include data files (schemas & workflow definitions) - Updated script which initialises the database to use the installed version of the library.

--- sip/execution_control/configuration_db/redis/config_db/__init__.py
+++ sip/execution_control/configuration_db/redis/config_db/__init__.py
@@ -1,7 +1,11 @@
 # coding=utf-8
 """SIP Execution Control Configuration Database client library."""
-# from .config_db_redis import ConfigDb
-# from .sdp_states import SDPStates
-# from .pb import ProcessingBlock
-# from .sbi import SchedulingBlockInstance
-# from .subarray import Subarray
+from .config_db_redis import ConfigDb
+from .pb import ProcessingBlock
+from .sbi import SchedulingBlockInstance
+from .pb_list import ProcessingBlockList
+from .sbi_list import SchedulingBlockInstanceList
+from .subarray import Subarray
+from .subarray_list import SubarrayList
+from .sdp_state import SDPState
+from .service_state import ServiceState
--- sip/execution_control/configuration_db/redis/setup.py
+++ sip/execution_control/configuration_db/redis/setup.py
@@ -1,15 +1,29 @@
+# coding: utf-8
 """Setup config file to package the configuration database."""
 from setuptools import setup
+import os
 
-setup(name='skasip-config_db',
-      version='0.4.0',
+
+def package_files(directory):
+    """Get list of data files to add to the package."""
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+data_files = package_files('config_db/utils/data')
+schema_files = package_files('config_db/schema')
+
+
+setup(name='skasip_config_db',
+      version='1.0.0',
       description='SIP Execution Control Configuration Database '
                   'client library.',
       author='SKA SIP',
-      packages=['config_db'],
-      install_requires=[
-          'redis',
-          'jsonschema',
-          'simplejson',
-          'namesgenerator'],
+      packages=['config_db', 'config_db/utils'],
+      package_data={'': data_files, '': schema_files},
+      include_package_data=True,
+      install_requires=['redis', 'jsonschema'],
       zip_safe=False)
