79f63ee622b3c64e95d8c075d10fb9c10d252260
Vladislav Stolyarov <vs237@ilab-gate.ilab.cluster>
2018-11-08 12:24:28 +0000

    The scripts to run ICAL pipeline on AlaSKA P3 cluster.

---
+++ sip/science_pipeline_workflows/ical_dask/scripts/DaskSwarmStartP3_bind.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script to create the Dask EE services needed to run the ICAL workflow.
+
+# Create overlay network (if required)
+NETWORK=ical_sip
+docker network create --driver overlay ${NETWORK} --attachable
+
+# Create Dask scheduler service and expose ports
+docker service create \
+    --name scheduler \
+    --network ${NETWORK} \
+    --publish 8786:8786 \
+    --publish 8787:8787 \
+    vlad7235/ical_dask_scheduler:latest
+
+# Create Dask worker service adding ARL library as a bind mount
+docker service create \
+    --name worker \
+    --network ${NETWORK} \
+    --mount type=bind,source=/var/mnt/ceph/vlad/sdp_arl,destination=/worker/sdp_arl \
+    --env ARL_DASK_SCHEDULER=scheduler:8786 \
+    --env PYTHONPATH=/worker/sdp_arl \
+    --replicas 2 \
+    vlad7235/ical_dask_worker:latest \
+    scheduler:8786 --nprocs 2 --nthreads 1 --memory-limit 2GB
