87a824d31c35ebdc42f50c7fd1ba75f2c075703f
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2018-11-15 23:13:35 +0000

    Test script for docker swarm api on p3

--- sip/execution_control/docker_api/docker_client/tests/test_docker_client.py
+++ sip/execution_control/docker_api/docker_client/tests/test_docker_client.py
@@ -90,24 +90,6 @@ def test_service_state():
     DC.delete_service("mock_stage")
 
 
-def test_log_driver():
-    """Test function to check if log driver is loaded correctly from
-    compose file.
-
-    """
-    config_path = os.path.join(FILE_PATH, '..', 'compose-file',
-                               'docker-compose.p3-fluentd.yml')
-    running_service_ids = []
-    test_ids = []
-    with open(config_path, 'r') as compose_str:
-        s_ids = DC.create_services(compose_str)
-        # TODO (NJT) nEED TO COMPLETE THIS UNIT TEST
-        # GET SERVICE DETAILS AND GET THE LOG DRIVE AND LOG OPTIONS FROM IT
-        # for s_id in s_ids:
-        #     running_service_ids.append(s_id)
-        #     test_ids.append(s_id)
-
-
 def test_get_service_list():
     """Test function for getting service list."""
     # Create new services
