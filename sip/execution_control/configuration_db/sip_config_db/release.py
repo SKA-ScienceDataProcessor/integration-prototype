deb79ee76e90188fb9d43d925b92d4eb2967901a
Benjamin Mort <bmort@users.noreply.github.com>
2019-01-09 10:38:38 +0000

    :arrow_up: Updated to PyYAML==4.2b (#67)
    - This addresses a security vulnerability in PyYAML==3.13 reported by
      GitHub security alerts.
    - `skasip-config-db` and `skasip-pbc` pypi libraries updated
      accordingly
    - Updated Docker images to the new python package versions resulting
      from this fix:
      - `skasip/processing_block_controller`
      - `skasip/processing_controller`

--- sip/execution_control/configuration_db/sip_config_db/release.py
+++ sip/execution_control/configuration_db/sip_config_db/release.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 """Release information for the SIP Execution Control Configuration Database."""
-__version_info__ = (1, 2, 1)
+__version_info__ = (1, 2, 2)
 __version__ = '.'.join(map(str, __version_info__))
 __sbi_version__ = "1.0.0"
 __pb_version__ = "1.0.0"
--- sip/execution_control/processing_controller/scheduler/release.py
+++ sip/execution_control/processing_controller/scheduler/release.py
@@ -2,7 +2,7 @@
 """Processing Controller release info."""
 __subsystem__ = 'ExecutionControl'
 __service_name__ = 'ProcessingController'
-__version_info__ = (1, 2, 3)
+__version_info__ = (1, 2, 4)
 __version__ = '.'.join(map(str, __version_info__))
 __service_id__ = ':'.join(map(str, (__subsystem__,
                                     __service_name__,
