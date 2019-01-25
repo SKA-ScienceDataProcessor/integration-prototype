64ae9fb338168968e6203a05c69a4a5da796b28f
Benjamin Mort <bmort@users.noreply.github.com>
2019-01-25 18:02:44 +0000

    Update to testing script.
    - Added the `--tests-only` option to the sip test test runner
      script to simply run unit tests and ignore linters and code
      coverage. This greatly speeds up running unit tests locally
      during development should this script be used.
    - Added ignore of `setup.py` files when assessing test coverage.

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -1,18 +1,38 @@
 #!/usr/bin/env bash
 # Run tests and linters for specified directory.
+#
+# Usage:
+#   ./tools/run_tests.sh <directory> [--test-only] [pytest flags]
+#
+# Example:
+#   ./tools/run_tests.sh sip/execution_control/configuration_db \
+#        --test-only -x -k test_workflow_definitions
+#
 RED='\033[0;31m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
 DIR="$1"
 echo -e "${RED}* DIR=${NC}'${DIR}'"
-OPTIONS="${@:2}"
-if [[ ! -z "${OPTIONS}" ]]; then
+OPTIONS=("${@:2}")
+if [[ ! -z "${OPTIONS}" && "${OPTIONS[0]}" == "--test-only" ]]; then
+    OPTIONS="${OPTIONS[@]:1}"
     echo -e "${RED}------------------------------------------------------${NC}"
     echo -e "${RED}* OPTIONS=${NC}'${OPTIONS}'"
     CMD="python3 -m pytest -s -v \
     --rootdir=. \
-    --pylint ${OPTIONS} \
+    ${OPTIONS[*]} \
+    ${DIR}"
+    echo -e "${RED}------------------------------------------------------${NC}"
+    echo -e "${BLUE}Running tests:"
+    echo -e "${BLUE}${CMD}"
+    echo -e "${RED}------------------------------------------------------${NC}"
+elif [[ ! -z "${OPTIONS}" ]]; then
+    echo -e "${RED}------------------------------------------------------${NC}"
+    echo -e "${RED}* OPTIONS=${NC}'${OPTIONS}'"
+    CMD="python3 -m pytest -s -v \
+    --rootdir=. \
+    --pylint \
     --pylint-rcfile=.pylintrc \
     --codestyle \
     --docstyle \
@@ -20,7 +40,9 @@ if [[ ! -z "${OPTIONS}" ]]; then
     --cov-append \
     --cov-branch \
     --no-cov-on-fail \
-    --cov=${DIR} ${DIR}"
+    --cov=${DIR} \
+    ${OPTIONS[*]} \
+    ${DIR}"
     echo -e "${RED}------------------------------------------------------${NC}"
     echo -e "${BLUE}Running tests:"
     echo -e "${BLUE}${CMD}"
