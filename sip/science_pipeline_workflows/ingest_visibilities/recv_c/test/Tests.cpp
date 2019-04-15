8def33cb5e05d4ba00e521143a48c7bcc2326459
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2019-04-15 22:50:59 +0100

    Initial commit of the feature/recv_unit_test branch

--- sip/science_pipeline_workflows/ingest_visibilities/recv_c/test/Tests.cpp
+++ sip/science_pipeline_workflows/ingest_visibilities/recv_c/test/Tests.cpp
@@ -107,3 +107,89 @@ TEST(Stream, test_stream_decode)
     receiver_free(receiver);
     free(buf);
 }
+
+TEST(Stream, test_stream_receive)
+{
+    /* Verify stream_decode() function and verify if correct
+    data is received */
+
+    // ADD DESCRIPTOR AND OTHER IDS
+    // SPEAD packet item IDs.
+    const unsigned int item_ids[] = {
+        0x1,    // Heap counter.
+        0x2,    // Heap size.
+        0x3,    // Heap offset.
+        0x6000,
+        0x6001
+        // 0x6005,
+        // 0x6008,
+        // 0x600A
+    };
+
+    // NEED TO FIGURE OUT HOW TO DO FLOOR IN C
+    // ADD DESCRIPTOR AND OTHER VALUES
+    //num_baselines = (512 * 513) // 2
+    // Dummy values for items.
+    const int item_val[] = {
+        4,
+        10,
+        55,
+        1,
+        0
+        
+    };
+    const unsigned int num_items = sizeof(item_ids) / sizeof(unsigned int);
+
+    // Allocate memory for a SPEAD packet.
+    unsigned char* buf = (unsigned char*) malloc(8 + num_items * 8);
+    ASSERT_TRUE(buf != NULL);
+
+    // Define SPEAD flavour.
+    const unsigned char heap_address_bits = 48;
+    const unsigned char item_id_bits = 64 - heap_address_bits - 1;
+
+    // Construct SPEAD packet header.
+    buf[0] = 'S';
+    buf[1] = 4;
+    buf[2] = (item_id_bits + 1) / 8;
+    buf[3] = heap_address_bits / 8;
+    buf[4] = 0;
+    buf[5] = 0;
+    buf[6] = 0;
+    buf[7] = (unsigned char) num_items;
+
+    // Construct SPEAD packet payload containing items in
+    // "immediate addressing mode".
+    uint64_t* items = (uint64_t*) &buf[8];
+    for (unsigned int i = 0; i < num_items; i++)
+    {
+        const uint64_t item_id = (uint64_t) item_ids[i];
+        const uint64_t item_addr_or_val = (uint64_t) item_val[i];
+        //printf("Item Value: %i\n", item_val[i]);
+        const uint64_t item =
+                (1ull << 63) |
+                (item_id << heap_address_bits) |
+                item_addr_or_val;
+        items[i] = htobe64(item);
+    }
+
+    // Create a test receiver.
+    struct Receiver* receiver = create_test_receiver();
+    ASSERT_TRUE(receiver != NULL);
+
+    // Call stream_decode().
+    stream_decode(receiver->streams[0], buf, 0);
+    printf("Heap count - %i\n", receiver->streams[0]->heap_count);
+    printf("BUffer Len - %i\n", receiver->streams[0]->buffer_len);
+    printf("Num buffers - %i\n", receiver->num_buffers);
+    printf("completed_streams - %i\n", receiver->completed_streams);
+    printf("max_num_buffers - %i\n", receiver->max_num_buffers);
+    printf("num_baselines - %i\n", receiver->num_baselines);
+    // printf("buffer_id - %i\n", receiver->buffers[0]->buffer_id);
+    // struct Buffer* buff = receiver->buffers[0];
+    // printf("Buf %i", buff->buffer_id);
+
+    // Clean up.
+    receiver_free(receiver);
+    free(buf);
+}
