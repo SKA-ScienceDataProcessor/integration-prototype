423170e5fefab2fe6d8d7a99cf86ca7f87afc89d
Andrew McDermott <andrew.mcdermott@stfc.ac.uk>
2019-04-25 13:19:00 +0100

    Changes to the test_stream_receive

--- sip/science_pipeline_workflows/ingest_visibilities/recv_c/test/Tests.cpp
+++ sip/science_pipeline_workflows/ingest_visibilities/recv_c/test/Tests.cpp
@@ -108,117 +108,167 @@ TEST(Stream, test_stream_decode)
     free(buf);
 }
 
-// TEST(Stream, test_stream_receive)
-// {
-//     /* Verify stream_decode() function and verify if correct
-//     data is received */
+typedef enum _item_ids {
+    heap_counter=0x01,
+    heap_size,
+    heap_offset,
+    packet_payload_length,
+    item_descriptor,
+    stream_control,
+    item_desc_name=0x10,
+    item_des_description,
+    item_desc_shape,
+    item_desc_type,
+    item_desc_ID,
+    item_desc_dtype,
+    visibility_timestamp_count=0x6000,
+    visibility_timestamp_fraction,
+    visibility_baseline_count_0x6005,
+    scan_ID=0x6008,
+    visibility_data=0x600A
+} item_id;
 
-//     // ADD DESCRIPTOR AND OTHER IDS
-//     // SPEAD packet item IDs.
-//     const unsigned int item_ids[] = {
-//         0x1,    // Heap counter.
-//         0x2,    // Heap size.
-//         0x3,    // Heap offset.
-//         0x4,    // packet payload length
-//         0x5,    // item descriptor
-//         0x6,    // stream control
-//         // 0x10,   // item descriptor: name
-//         // 0x11,   // item descriptor: description
-//         // 0x12,   // item descriptor: shape
-//         // 0x13,   // item descriptor: type
-//         // 0x14,   // item descriptor: ID
-//         // 0x15,   // item descriptor: dtype  
-//         0x6000, // Visibility timestamp count
-//         0x6001, // Visibility timestamp fraction
-//         0x6005, // Visibility baseline count
-//         0x6008, // Scan ID 
-//         0x600A  // Visibility data 
+typedef struct _item_block {
+    item_id id;
+    uint64_t val;
+    uint64_t imm;
+} item_block;
 
-//     };
+TEST(Stream, test_stream_receive)
+{
+    /* Verify stream_decode() function and verify if correct
+    data is received */
 
-//     // NEED TO FIGURE OUT HOW TO DO FLOOR IN C
-//     // ADD DESCRIPTOR AND OTHER VALUES
-//     // int num_baselines = (512 * 513) // 2
-//     // Dummy values for items.
+    // ADD DESCRIPTOR AND OTHER IDS
+    // SPEAD packet item IDs.
+    // *** APM: No longer required?
+    //~ const unsigned int item_ids[] = {
+        //~ 0x1,    // Heap counter.
+        //~ 0x2,    // Heap size.
+        //~ 0x3,    // Heap offset.
+        //~ 0x4,    // packet payload length
+        //~ 0x5,    // item descriptor
+        //~ 0x6,    // stream control
+        //~ // 0x10,   // item descriptor: name
+        //~ // 0x11,   // item descriptor: description
+        //~ // 0x12,   // item descriptor: shape
+        //~ // 0x13,   // item descriptor: type
+        //~ // 0x14,   // item descriptor: ID
+        //~ // 0x15,   // item descriptor: dtype  
+        //~ 0x6000, // Visibility timestamp count
+        //~ 0x6001, // Visibility timestamp fraction
+        //~ 0x6005, // Visibility baseline count
+        //~ 0x6008, // Scan ID 
+        //~ 0x600A  // Visibility data 
 
-//     // # Update values in the heap.
-//     // item_group['visibility_timestamp_count'].value = 1
-//     // item_group['visibility_timestamp_fraction'].value = 0
-//     // item_group['visibility_baseline_count'].value = num_baselines
-//     // item_group['scan_id'].value = 100000000
-//     // item_group['correlator_output_data'].value = vis
+    //~ };
 
-//     // Alternative to this in C
-//     // vis = numpy.zeros(shape=(num_baselines,), dtype=dtype)
+    // NEED TO FIGURE OUT HOW TO DO FLOOR IN C
+    // ADD DESCRIPTOR AND OTHER VALUES
+    // int num_baselines = (512 * 513) // 2
+    // Dummy values for items.
 
-//     const int item_val[] = {
-//         4,
-//         10,
-//         55,
-//         55,
-//         0,
-//         1,              // Visibility timestamp count
-//         0,              // Visibility timestamp fraction    
-//         // num_baselines,  // Visibility baseline count
-//         100000000,      // Scan ID 
-//         // vis             // Visibility data 
+    // # Update values in the heap.
+    // item_group['visibility_timestamp_count'].value = 1
+    // item_group['visibility_timestamp_fraction'].value = 0
+    // item_group['visibility_baseline_count'].value = num_baselines
+    // item_group['scan_id'].value = 100000000
+    // item_group['correlator_output_data'].value = vis
 
-//         // 1,
-//         // 0
+    // Alternative to this in C
+    // vis = numpy.zeros(shape=(num_baselines,), dtype=dtype)
+
+    // *** APM: No longer required?
+    //~ const int item_val[] = {
+        //~ 4,
+        //~ 10,
+        //~ 55,
+        //~ 55,
+        //~ 0,
+        //~ 1,              // Visibility timestamp count
+        //~ 0,              // Visibility timestamp fraction    
+        //~ // num_baselines,  // Visibility baseline count
+        //~ 100000000,      // Scan ID 
+        //~ // vis             // Visibility data 
+
+        //~ // 1,
+        //~ // 0
         
-//     };
-//     const unsigned int num_items = sizeof(item_ids) / sizeof(unsigned int);
+    //~ };
 
-//     // Allocate memory for a SPEAD packet.
-//     unsigned char* buf = (unsigned char*) malloc(8 + num_items * 8);
-//     ASSERT_TRUE(buf != NULL);
 
-//     // Define SPEAD flavour.
-//     const unsigned char heap_address_bits = 48;
-//     const unsigned char item_id_bits = 64 - heap_address_bits - 1;
+    // *** APM: This const array contains item-id, 
+    //          item value and immediate flag.
+    const item_block p_items[] = { 
+	{heap_counter, 4, 1}, 
+	{heap_size, 10, 1},
+	{heap_offset, 55, 1}, 
+	{packet_payload_length, 55, 1},
+	{item_descriptor, 0, 0}, 
+	{stream_control, 1, 1},
+	{visibility_timestamp_count, 1, 1}, 
+	{visibility_timestamp_fraction, 0, 1},
+	{scan_ID, 100000000, 0}
+    };
+    const unsigned int num_items = sizeof(p_items) / sizeof(item_block);
+    
+    uint64_t heap_count;
 
-//     // Construct SPEAD packet header.
-//     buf[0] = 'S';
-//     buf[1] = 4;
-//     buf[2] = (item_id_bits + 1) / 8;
-//     buf[3] = heap_address_bits / 8;
-//     buf[4] = 0;
-//     buf[5] = 0;
-//     buf[6] = 0;
-//     buf[7] = (unsigned char) num_items;
+    // Allocate memory for a SPEAD packet.
+    unsigned char* buf = (unsigned char*) malloc(8 + num_items * 8);
+    ASSERT_TRUE(buf != NULL);
 
-//     // Construct SPEAD packet payload containing items in
-//     // "immediate addressing mode".
-//     uint64_t* items = (uint64_t*) &buf[8];
-//     for (unsigned int i = 0; i < num_items; i++)
-//     {
-//         const uint64_t item_id = (uint64_t) item_ids[i];
-//         const uint64_t item_addr_or_val = (uint64_t) item_val[i];
-//         //printf("Item Value: %i\n", item_val[i]);
-//         const uint64_t item =
-//                 (1ull << 63) |
-//                 (item_id << heap_address_bits) |
-//                 item_addr_or_val;
-//         items[i] = htobe64(item);
-//     }
+    // Define SPEAD flavour.
+    const unsigned char heap_address_bits = 48;
+    const unsigned char item_id_bits = 64 - heap_address_bits - 1;
 
-//     // Create a test receiver.
-//     struct Receiver* receiver = create_test_receiver();
-//     ASSERT_TRUE(receiver != NULL);
+    // Construct SPEAD packet header.
+    buf[0] = 'S';
+    buf[1] = 4;
+    buf[2] = (item_id_bits + 1) / 8;
+    buf[3] = heap_address_bits / 8;
+    buf[4] = 0;
+    buf[5] = 0;
+    buf[6] = 0;
+    buf[7] = (unsigned char) num_items;  // Luckily < 256 items :-)
 
-//     // Call stream_decode().
-//     stream_decode(receiver->streams[0], buf, 0);
-//     printf("Heap count - %i\n", receiver->streams[0]->heap_count);
-//     printf("BUffer Len - %i\n", receiver->streams[0]->buffer_len);
-//     printf("Num buffers - %i\n", receiver->num_buffers);
-//     printf("completed_streams - %i\n", receiver->completed_streams);
-//     printf("max_num_buffers - %i\n", receiver->max_num_buffers);
-//     printf("num_baselines - %i\n", receiver->num_baselines);
-//     // printf("buffer_id - %i\n", receiver->buffers[0]->buffer_id);
-//     // struct Buffer* buff = receiver->buffers[0];
-//     // printf("Buf %i", buff->buffer_id);
+    // Construct SPEAD packet payload containing items in
+    // "immediate addressing mode".
+    uint64_t* items = (uint64_t*) &buf[8];
+    for (unsigned int i = 0; i < num_items; i++)
+    {
+        const uint64_t item_id = (uint64_t) p_items[i].id;
+        const uint64_t item_addr_or_val = (uint64_t) p_items[i].val;
+	const uint64_t imm_or_abs = p_items[i].imm << 63;
+        //printf("Item Value: %i\n", item_val[i]);
+        const uint64_t item =
+                imm_or_abs |
+                (item_id << heap_address_bits) |
+                item_addr_or_val;
+        items[i] = htobe64(item);
+	switch (p_items[i].id)
+	{
+	    case heap_counter : heap_count = p_items[i].val; break;
+	}
+    }
 
-//     // Clean up.
-//     receiver_free(receiver);
-//     free(buf);
-// }
+    // Create a test receiver.
+    struct Receiver* receiver = create_test_receiver();
+    ASSERT_TRUE(receiver != NULL);
+
+    // Call stream_decode().
+    stream_decode(receiver->streams[0], buf, 0);
+    printf("Heap count - %i\n", receiver->streams[0]->heap_count); // could assert heap count value here
+    printf("Buffer Len - %i\n", receiver->streams[0]->buffer_len); // could assert default values here
+    printf("Num buffers - %i\n", receiver->num_buffers);
+    printf("completed_streams - %i\n", receiver->completed_streams);
+    printf("max_num_buffers - %i\n", receiver->max_num_buffers);
+    printf("num_baselines - %i\n", receiver->num_baselines);
+    // printf("buffer_id - %i\n", receiver->buffers[0]->buffer_id);
+    // struct Buffer* buff = receiver->buffers[0];
+    // printf("Buf %i", buff->buffer_id);
+
+    // Clean up.
+    receiver_free(receiver);
+    free(buf);
+}
