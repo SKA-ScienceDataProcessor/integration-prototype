ad4d69741c1ba8502180592b0a84c96cfde48c7f
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2019-04-25 11:37:40 +0100

    Updated travis to include codecov v3

--- tools/build_test.sh
+++ tools/build_test.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+set -euo pipefail
+
 mkdir sip/science_pipeline_workflows/ingest_visibilities/recv_c/build
 cd sip/science_pipeline_workflows/ingest_visibilities/recv_c/build
-cmake ..
-cmake --build .
-./test/recv_test
-gcov-5 sip/science_pipeline_workflows/ingest_visibilities/recv_c/src/*.{c,h}
-gcov-5 sip/science_pipeline_workflows/ingest_visibilities/recv_c/test/Tests.cpp
+#cmake ..
+
+# Configure
+cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
+cmake --build . --config Debug -- -j $(nproc)
+# cmake --build .
+
+# Test
+ctest -j $(nproc) --output-on-failure
+# ./test/recv_test
