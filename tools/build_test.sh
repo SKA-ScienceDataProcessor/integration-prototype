0c5ff002c27624cd29a7b533837a470d140c26a9
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2019-04-25 11:19:42 +0100

    Updated travis to include codecov for the c unit test v2

--- tools/build_test.sh
+++ tools/build_test.sh
@@ -5,3 +5,5 @@ cd sip/science_pipeline_workflows/ingest_visibilities/recv_c/build
 cmake ..
 cmake --build .
 ./test/recv_test
+gcov-5 sip/science_pipeline_workflows/ingest_visibilities/recv_c/src/*.{c,h}
+gcov-5 sip/science_pipeline_workflows/ingest_visibilities/recv_c/test/Tests.cpp
