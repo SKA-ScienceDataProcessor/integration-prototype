9362f9d574d077ae04d3ad370eae64191f176a63
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2019-05-09 15:52:22 +0100

    UKSW-9 updated travis.ci with coveralls v1

---
+++ tools/run_c_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir sip/science_pipeline_workflows/ingest_visibilities/recv_c/build 
+cd sip/science_pipeline_workflows/ingest_visibilities/recv_c/build 
+cmake -DCOVERALLS=ON -DCMAKE_BUILD_TYPE=Debug ..
+cmake --build . && cmake --build . --target coveralls
+./test/recv_test
