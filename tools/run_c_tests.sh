c454dd4b6d9f71e9e3d1a47ce8298f70093bf60f
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2019-05-14 10:39:28 +0100

    UKSW-29 added cppcheck to the travis and cmakelists.txt

--- tools/run_c_tests.sh
+++ tools/run_c_tests.sh
@@ -2,7 +2,7 @@
 
 mkdir sip/science_pipeline_workflows/ingest_visibilities/recv_c/build 
 cd sip/science_pipeline_workflows/ingest_visibilities/recv_c/build 
-cmake -DCOVERALLS=ON -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCOVERALLS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_CPPCHECK:FILEPATH=cppcheck .. 
 cmake --build .
 ./test/recv_test
 cmake --build . --target coveralls
