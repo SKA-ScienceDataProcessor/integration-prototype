83a0c3d17f271ab3e3bb086d656abf40fc937f2d
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2019-05-14 10:56:27 +0100

    UKSW-29 updated run_c_tests.sh

--- tools/run_c_tests.sh
+++ tools/run_c_tests.sh
@@ -2,7 +2,9 @@
 
 mkdir sip/science_pipeline_workflows/ingest_visibilities/recv_c/build 
 cd sip/science_pipeline_workflows/ingest_visibilities/recv_c/build 
-cmake -DCOVERALLS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_CPPCHECK:FILEPATH=cppcheck .. 
+cmake -DCOVERALLS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_CPPCHECK:FILEPATH=cppcheck ..
+echo "Building and running cppcheck\n" 
 cmake --build .
+echo "Running Unit Test\n" 
 ./test/recv_test
 cmake --build . --target coveralls
