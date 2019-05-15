b1fdb1a17417c86057b98f48883c19f3eef8c870
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2019-05-15 09:50:45 +0100

    UKSW-29 using cppcheck project, didnt work as expected. Therefore changed to using cppcheck without --project

--- tools/run_c_tests.sh
+++ tools/run_c_tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir sip/science_pipeline_workflows/ingest_visibilities/recv_c/build 
-cd sip/science_pipeline_workflows/ingest_visibilities/recv_c/build 
-cmake -DCOVERALLS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_CPPCHECK:FILEPATH=cppcheck ..
-echo "Building and running cppcheck\n" 
-cmake --build .
-echo "Running Unit Test\n" 
+cd sip/science_pipeline_workflows/ingest_visibilities/
+cppcheck recv_c/ -i gtest/ --enable=warning,portability,style
+mkdir recv_c/build
+cd recv_c/build
+cmake -DCOVERALLS=ON -DCMAKE_BUILD_TYPE=Debug ..
+cmake --build . 
 ./test/recv_test
 cmake --build . --target coveralls
