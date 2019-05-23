157056b592c8a7ca83b570977341b81c386e23c4
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2019-05-23 13:07:07 +0100

    Added address sanitizer to the ingest script

--- tools/run_c_tests.sh
+++ tools/run_c_tests.sh
@@ -4,7 +4,7 @@ cd sip/science_pipeline_workflows/ingest_visibilities/
 cppcheck recv_c/ -i gtest/ --enable=warning,portability,style
 mkdir recv_c/build
 cd recv_c/build
-cmake -DCOVERALLS=ON -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCOVERALLS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DADDRESS_SANITIZER=On ..
 cmake --build .
 ctest -T memcheck
 ./test/recv_test
