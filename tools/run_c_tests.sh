31a53845db8075e7814acc30cbb735197a5578d3
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2019-05-29 16:13:07 +0100

    UKSW-29 disabled coveralls

--- tools/run_c_tests.sh
+++ tools/run_c_tests.sh
@@ -4,8 +4,6 @@ cd sip/science_pipeline_workflows/ingest_visibilities/
 cppcheck recv_c/ -i gtest/ --enable=warning,portability,style
 mkdir recv_c/build
 cd recv_c/build
-cmake -DCOVERALLS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DADDRESS_SANITIZER=On ..
-cmake --build .
-ctest -T memcheck
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DADDRESS_SANITIZER=On ..
+make
 ./test/recv_test
-cmake --build . --target coveralls
