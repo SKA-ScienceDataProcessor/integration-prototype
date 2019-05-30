93d31a5363ffae1edcd51bcfd3197ae2fc9c6092
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2019-05-30 10:17:12 +0100

    adding coveralls target

--- tools/run_c_tests.sh
+++ tools/run_c_tests.sh
@@ -4,7 +4,7 @@ cd sip/science_pipeline_workflows/ingest_visibilities/
 cppcheck recv_c/ -i gtest/ --enable=warning,portability,style
 mkdir recv_c/build
 cd recv_c/build
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DADDRESS_SANITIZER=On ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DADDRESS_SANITIZER=On -DCOVERALLS=ON ..
 make
 ./test/recv_test
 cmake --build . --target coveralls
