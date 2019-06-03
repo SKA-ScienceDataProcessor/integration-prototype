55ba63a6b68a89b94e68d8741b755ab52c542a97
njthykkathu <nijin.thykkathu@stfc.ac.uk>
2019-06-03 11:52:57 +0100

    UKSW-10 Unit tests for visibility receive (continued) (#97)
    * Initial commit of the feature/recv_unit_test branch
    
    * current version of test.cpp for andrew to work on
    
    * Testing extended travis.ci with cpp
    
    * Testing extended travis.ci with cpp
    
    * More testing
    
    * More testing
    
    * More testing
    
    * Updated travis to include codecov for the c unit test
    
    * Updated travis to include codecov for the c unit test
    
    * Updated travis to include codecov for the c unit test v2
    
    * Updated travis to include codecov v3
    
    * Updated travis to include codecov v4
    
    * Changes to the test_stream_receive
    
    * Updated ingest unit test, more work to be done on visibility data
    
    * temp commit
    
    * Updated unit test includes heap data
    
    * Vis recv unit test completed
    
    * Update sip/science_pipeline_workflows/ingest_visibilities/recv_c/src/stream.c
    
    Co-Authored-By: apmcd42 <andrew.mcdermott@stfc.ac.uk>
    
    * Modifications after comments by Fred
    
    * Revert to be the same version as in the main branch
    
    * TSK-2774 Add unit tests for C receiver. Final commit?
    
    * UKSW-10 Committing a new file called note. Might need to come back to it later
    
    * Updated jinja2 dependency to address security alert [UKSW-20] (#98)
    
    * Initial commit of the feature/recv_unit_test branch
    
    * Testing extended travis.ci with cpp
    
    * More testing
    
    * More testing
    
    * More testing
    
    * Updated travis to include codecov for the c unit test
    
    * Updated travis to include codecov for the c unit test
    
    * Updated travis to include codecov for the c unit test v2
    
    * Updated travis to include codecov v3
    
    * Updated travis to include codecov v4
    
    * UKSW-9 Travis updated with coveralls v3
    
    * Fix memory leak in receiver_free().
    
    * UKSW-10 Committing a new file called note. Might need to come back to it later
    
    * UKSW-10 Adding compiler
    
    * UKSW-10 Upated cmake standard

--- sip/science_pipeline_workflows/ingest_visibilities/recv_c/test/Tests.cpp
+++ sip/science_pipeline_workflows/ingest_visibilities/recv_c/test/Tests.cpp
@@ -10,6 +10,7 @@
 
 #include "receiver.h"
 #include "stream.h"
+#include "buffer.h"
 
 #include <cstdio>
 #include <inttypes.h>
@@ -102,6 +103,120 @@ TEST(Stream, test_stream_decode)
 
     // Call stream_decode().
     stream_decode(receiver->streams[0], buf, 0);
+
+    // Clean up.
+    receiver_free(receiver);
+    free(buf);
+}
+
+TEST(Stream, test_stream_receive)
+{
+    /* Verify stream_decode() function and verify if correct
+    data is received */
+    typedef enum _item_ids {
+        heap_counter=0x01,
+        heap_size,
+        heap_offset,
+        packet_payload_length,
+        visibility_baseline_count=0x6005,
+        scan_ID=0x6008,
+        visibility_data=0x600A
+    } item_id;
+
+    typedef struct _item_block {
+        item_id id;
+        uint64_t val;
+        uint64_t imm;
+    } item_block;
+
+    int num_baselines = 512 *513 / 2;
+    struct  DataType vis_data = {0,0,{{1.5,2.5},{3.5,4.5},{5.5,6.5},{7.5,8.5}}};
+    uint64_t scan_id=0, *scan_ptr=NULL;
+    void *heap_start;
+    struct DataType * vis_ptr;
+    size_t heap_sz = 8 + sizeof(vis_data);
+
+    const item_block p_items[] = { 
+	{heap_counter, 2, 1}, 
+	{heap_size, (uint64_t) heap_sz, 1},
+	{heap_offset, 0, 1}, 
+	{packet_payload_length, (uint64_t) heap_sz, 1},
+	{visibility_baseline_count, (uint64_t) htobe32((uint32_t)num_baselines), 1},
+	{scan_ID, 0, 0},
+	{visibility_data, 8, 0}
+    };
+    
+    const unsigned int num_items = sizeof(p_items) / sizeof(item_block);
+
+    // Allocate memory for a SPEAD packet.
+    unsigned char* buf = (unsigned char*) malloc(8 + num_items * 8 + heap_sz);
+    ASSERT_NE(buf, nullptr);
+
+    // Define SPEAD flavour.
+    const unsigned char heap_address_bits = 48;
+    const unsigned char item_id_bits = 64 - heap_address_bits - 1;
+
+    // Construct SPEAD packet header.
+    buf[0] = 'S';
+    buf[1] = 4;
+    buf[2] = (item_id_bits + 1) / 8;
+    buf[3] = heap_address_bits / 8;
+    buf[4] = 0;
+    buf[5] = 0;
+    buf[6] = 0;
+    buf[7] = (unsigned char) num_items; 
+
+    // Construct SPEAD packet payload containing items in
+    // "immediate addressing mode".
+    uint64_t* items = (uint64_t*) &buf[8];
+    for (unsigned int i = 0; i < num_items; i++)
+    {
+        const uint64_t item_id = (uint64_t) p_items[i].id;
+        const uint64_t item_addr_or_val = (uint64_t) p_items[i].val;
+        const uint64_t imm_or_abs = p_items[i].imm << 63;
+        const uint64_t item =
+                imm_or_abs |
+                (item_id << heap_address_bits) |
+                item_addr_or_val;
+        items[i] = htobe64(item);
+	/* APM - the following is for picking up any values we may wish
+	 * to use for testing later. Place a star-slash at
+	 * the end of this line to include the below code. 
+	switch (p_items[i].id)
+	{
+	    // heap_count will need to be defined.
+	    case heap_counter : heap_count = p_items[i].val; break;
+	} /* */
+    }
+
+    // get start of heap
+    heap_start = (void *)&items[num_items];
+
+    // insert scan_id at heap_start, 8 bytes
+    scan_ptr = (uint64_t *) heap_start;
+    // APM - Should it be this:
+    *scan_ptr = scan_id;
+    // ... or this?
+    // *scan_ptr = htobe64(scan_id);
+    vis_ptr = (struct DataType *) (scan_ptr+1);
+    memcpy((void *)vis_ptr, (void *) &vis_data, sizeof(vis_data));
+
+    // Create a test receiver.
+    struct Receiver* receiver = create_test_receiver();
+    ASSERT_NE(receiver, nullptr);
+
+    // Passing the buffer and stream to stream decode
+    stream_decode(receiver->streams[0], buf, 0);
+
+    // Testing
+    ASSERT_NE(receiver->streams[0], nullptr);
+    ASSERT_NE(receiver->buffers[0], nullptr);
+    ASSERT_NE(receiver->num_buffers, 0);
+    EXPECT_EQ(num_baselines, receiver->buffers[0]->num_baselines);
+    EXPECT_EQ(vis_ptr->fd, receiver->buffers[0]->vis_data->fd);
+    EXPECT_EQ(vis_ptr->tci, receiver->buffers[0]->vis_data->tci);
+    EXPECT_FLOAT_EQ(vis_ptr->vis[1].x, receiver->buffers[0]->vis_data->vis[1].x);
+    EXPECT_FLOAT_EQ(vis_ptr->vis[2].y, receiver->buffers[0]->vis_data->vis[2].y);
 
     // Clean up.
     receiver_free(receiver);
